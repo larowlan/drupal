f4cc488efaf056e7754ad9cd6ee76abae2c97c8e
Nick Schuch <nick@myschuch.com>
2014-05-24 23:14:09 +1000

    Adds test coverage.

--- core/modules/simpletest/lib/Drupal/simpletest/Tests/MinkNodeElementDecoratorTest.php
+++ core/modules/simpletest/lib/Drupal/simpletest/Tests/MinkNodeElementDecoratorTest.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\simpletest\Tests;
 
+use Drupal\simpletest\MinkNodeElementDecorator;
 use Drupal\simpletest\WebTestBase;
 
 /**
@@ -18,6 +19,13 @@ use Drupal\simpletest\WebTestBase;
  */
 class MinkNodeElementDecoratorTest extends WebTestBase {
 
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('mink_test');
+
   public static function getInfo() {
     return array(
       'name' => 'Mink node element decorator',
@@ -26,23 +34,31 @@ class MinkNodeElementDecoratorTest extends WebTestBase {
     );
   }
 
-  protected function setUp() {
-    $modules = array('mink_test');
-    parent::setUp($modules);
-    $this->initMink();
-  }
-
   /**
    * Test decorator.
    *
    * @covers MinkNodeElementDecorator::__get
    */
   public function testDecorator() {
-    $session = $this->getSession();
-    $session->visit('/mink-test-1');
-    $content = $session->getPage()->getContent();
-    $a = 1;
-    $this->assertRaw('adsflkajdsf', 'adsflkajdsf');
+    $this->drupalGet("/mink-test-1");
+    $element = $this->getSession()->getPage();
+    $container = $element->find("css", "#test-lists");
+    $container = new MinkNodeElementDecorator($container);
+
+    // Look for a multiple lists on the page. Should return an array of objects
+    // since that list has children (list items).
+    $this->assertTrue(is_array($container->ul), "Array of objects found.");
+
+    // Look for list with single list item. Should be a text value return.
+    $list = $container->ul[0];
+    $this->assertText($list->li, "item1", "Found the single item in the list.");
+
+    // Look for a list with multiple list items. Should return an array of
+    // strings.
+    $list = $container->ul[1];
+    $this->assertTrue(is_array($list->li));
+    $this->assertText($list->li[0], "item1", "Found the first item in the list.");
+    $this->assertText($list->li[1], "item2", "Found the second item in the list.");
   }
 
 }
